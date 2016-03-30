633dda4f50fb87d86fe6b53c82d33b642b2145ba
siddharth-111 <siddharth.kumar111@gmail.com>
2016-03-30 19:17:07 +0530

    3/30/16

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,9 +1,18 @@
  // When the browser is ready...
   $(function() {
-  
+
+
+$("#DOB").datepicker({
+         "autoclose": true,
+     orientation: "auto bottom"
+ });
+
+
     // Setup form validation on the #register-form element
     $("#register-form").validate({
-    
+         errorPlacement: function (error, element) {
+            error.appendTo(".errors");
+        },
         // Specify the validation rules
         rules: {
             Name: "required", 
@@ -52,7 +61,17 @@
             }
            
         },
-      
+         showErrors: function (errorMap, errorList) {
+            $("#register-form").find("input").each(function () {
+                $(this).removeClass("error");
+            });
+            $(".errors").html("");
+            if (errorList.length) {
+                $(errorList[0]['element']).addClass("error");
+                var elem = $(errorList[0]['element']).next();
+                elem.html(errorList[0]['message']);
+            }
+        },
         submitHandler: function(form) {
             form.submit();
         }
